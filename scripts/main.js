1e4034c74657bac8eacc93a5431de82436b8d087
alepascal <alejandro.padilla@gmail.com>
2025-05-05 14:06:52 -0600

    Fixed edge case where when seconds is equal to 9 it'd not format correctly for display

--- scripts/main.js
+++ scripts/main.js
@@ -4,43 +4,43 @@ let isTimerOn = false;
 let intervalId;
 const startButton = document.getElementById("start-button");
 const stopButton = document.getElementById("stop-button");
-const divContainerTimer = document.getElementsByClassName("timer__minutes")
+const divContainerTimer = document.getElementsByClassName("timer__minutes");
 console.log(divContainerTimer[0].firstElementChild.textContent)
 console.log(divContainerTimer[0].lastElementChild)
 
 const updateTime = () => {
     
     minutes = parseInt(minutes.slice(0, 2));
-    seconds = parseInt(seconds.slice(0, seconds.length === 2 ? 2 : 1));
+    seconds = parseInt(seconds.slice(0, 2));
+
+    console.log(`minutes before comparison: ${minutes}`)
+    console.log(`seconds before comparison: ${seconds}`)
 
     if(isTimerOn) {
-        if((seconds > 0 && seconds <= 9)
-        && (minutes > 0 && minutes <= 9)) {
-            seconds -= 1
-            minutes = "0" + String(minutes) // formatting with leading zero when > 0 and <= 9
-            seconds = "0" + String(seconds) // formatting with leading zero when > 0 and <= 9
-        } else if((seconds > 0 && seconds <= 9)
-        && (minutes >= 10 && minutes <= 25)) {
-            seconds -= 1
-            seconds = "0" + String(seconds)
-        } 
-        else if(seconds >= 10 && seconds <= 59) {
-            seconds -= 1
+        if(seconds > 0 && seconds <= 10) {
+            seconds -= 1;
+            seconds = "0" + String(seconds); // formatting with leading zero when > 0 and <= 9
+            if(minutes > 0 && minutes < 10) {
+                minutes = "0" + String(minutes); // formatting with leading zero when > 0 and < 10
+            }
+        } else if((seconds > 10)
+        && (minutes >= 10)) {
+            seconds -= 1;
         // case when a minute has passed          
         } else if (seconds == 0) {
-            minutes -= 1
-            seconds = 59
+            minutes -= 1;
+            seconds = 59;
         }
     }
     console.log(`minutes: ${minutes}`)
     console.log(`seconds: ${seconds}`)
     // set up the new text content in the document
 
-    minutes = String(minutes)
-    seconds = String(seconds)
-    const sep = ":"
-    divContainerTimer[0].firstElementChild.textContent = minutes + sep
-    divContainerTimer[0].lastElementChild.textContent = seconds
+    minutes = String(minutes);
+    seconds = String(seconds);
+    const sep = ":";
+    divContainerTimer[0].firstElementChild.textContent = minutes + sep;
+    divContainerTimer[0].lastElementChild.textContent = seconds;
 };
 
 startButton.addEventListener("click", () => {
